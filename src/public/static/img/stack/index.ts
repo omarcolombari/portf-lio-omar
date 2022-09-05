dc8822244c6f5af69fb2e325e42dee4c16011049
Omar <omarucolombari@gmail.com>
2022-09-05 14:12:00 -0300

    Feat: atualizado tecnologias

---
+++ src/public/static/img/stack/index.ts
@@ -0,0 +1,49 @@
+import imgHTML from "./html5.svg";
+import imgCSS from "./css3.svg";
+import imgJS from "./javascript.svg";
+import imgNode from "./nodedotjs.svg";
+import imgStyled from "./styledcomponents.svg";
+import imgReact from "./react.svg";
+import imgNext from "./next-js.svg";
+import imgTypeScript from "./typescript.svg";
+import imgDocker from "./docker.svg";
+import imgJson from "./json.svg";
+import imgVercel from "./vercel.svg";
+import imgExpress from "./express.svg";
+import imgJWT from "./jsonwebtokens.svg";
+import imgPostgres from "./postgresql.svg";
+import imgYarn from "./yarn.svg";
+import imgNpm from "./npm.svg";
+import imgPython from "./python.svg";
+import imgSlack from "./slack.svg";
+import imgHeroku from "./heroku.svg";
+import imgDjango from "./django.svg";
+import imgTrello from "./trello.svg";
+import imgJira from "./jirasoftware.svg";
+import imgVite from "./vite.svg";
+
+export {
+  imgCSS,
+  imgDocker,
+  imgExpress,
+  imgHTML,
+  imgJS,
+  imgJWT,
+  imgJson,
+  imgNext,
+  imgNode,
+  imgNpm,
+  imgPostgres,
+  imgReact,
+  imgStyled,
+  imgTypeScript,
+  imgVercel,
+  imgYarn,
+  imgPython,
+  imgSlack,
+  imgDjango,
+  imgHeroku,
+  imgTrello,
+  imgJira,
+  imgVite,
+};
--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -1,12 +1,31 @@
-import imgHTML from "../public/static/img/stack/html.svg";
-import imgCSS from "../public/static/img/stack/css.svg";
-import imgJS from "../public/static/img/stack/js.svg";
-import imgNode from "../public/static/img/stack/node.svg";
-import imgStyled from "../public/static/img/stack/styled.svg";
-import imgReact from "../public/static/img/stack/react.svg";
 import { FaVuejs } from "react-icons/fa";
 import { SiTypescript } from "react-icons/si";
 import { FaDocker } from "react-icons/fa";
+import {
+  imgHTML,
+  imgCSS,
+  imgDocker,
+  imgExpress,
+  imgJS,
+  imgJWT,
+  imgJson,
+  imgNext,
+  imgNode,
+  imgNpm,
+  imgPostgres,
+  imgReact,
+  imgStyled,
+  imgTypeScript,
+  imgVercel,
+  imgYarn,
+  imgPython,
+  imgDjango,
+  imgHeroku,
+  imgSlack,
+  imgTrello,
+  imgJira,
+  imgVite,
+} from "@/public/static/img/stack";
 
 export const stackData = [
   {
@@ -22,18 +41,83 @@ export const stackData = [
     img: imgJS,
   },
   {
+    title: "TypeScript",
+    img: imgTypeScript,
+  },
+  {
+    title: "Json",
+    img: imgJson,
+  },
+  {
     title: "Node JS",
     img: imgNode,
+  },
+  {
+    title: "React",
+    img: imgReact,
   },
   {
     title: "Styled Components",
     img: imgStyled,
   },
   {
-    title: "React",
-    img: imgReact,
+    title: "Next.js",
+    img: imgNext,
   },
-  { title: "Vuejs", img: FaVuejs },
-  { title: "TypeScript", img: SiTypescript },
-  { title: "Docker", img: FaDocker },
+  {
+    title: "Vite",
+    img: imgVite,
+  },
+  {
+    title: "Vercel",
+    img: imgVercel,
+  },
+  {
+    title: "Express",
+    img: imgExpress,
+  },
+  {
+    title: "JWT",
+    img: imgJWT,
+  },
+  {
+    title: "PostgreSQL",
+    img: imgPostgres,
+  },
+  {
+    title: "Yarn",
+    img: imgYarn,
+  },
+  {
+    title: "Npm",
+    img: imgNpm,
+  },
+  {
+    title: "Docker",
+    img: imgDocker,
+  },
+  {
+    title: "Python",
+    img: imgPython,
+  },
+  {
+    title: "Django",
+    img: imgDjango,
+  },
+  {
+    title: "Heroku",
+    img: imgHeroku,
+  },
+  {
+    title: "Slack",
+    img: imgSlack,
+  },
+  {
+    title: "Trello",
+    img: imgTrello,
+  },
+  {
+    title: "Jira",
+    img: imgJira,
+  },
 ];
